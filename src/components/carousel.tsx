3566a0a214cbcf79a11a9bd7e103e75eba6b33ff
José Lopes <eu@joselopes.me>
2025-02-12 14:52:00 -0300

    fix: issues and new configs light theme

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo } from 'react'
 
 import Image from 'next/image'
 import { Autoplay, EffectCards, EffectCoverflow, EffectCube, EffectFlip, Pagination } from 'swiper/modules'
