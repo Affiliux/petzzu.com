fa660025e86f7606c2e8d0619d7e7a0dbd6161fe
nachef <nathanvic.2001@gmail.com>
2025-02-18 23:39:19 -0300

    refactor: simplify conditional rendering in Page component for DefaultTheme

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -75,9 +75,7 @@ export default function Page({ params }: { params: Promise<{ slug: string }> }) 
       )}
 
       {couple && !loading && (
-        <>
-          {couple.themeShowType === ThemeShowTypeEnum.DEFAULT && <DefaultTheme couple={couple} />}
-        </>
+        <>{couple.themeShowType === ThemeShowTypeEnum.DEFAULT && <DefaultTheme couple={couple} />}</>
       )}
 
       {couple && (success || payment || (!view && couple.themeShowType === ThemeShowTypeEnum.DEFAULT)) && (
