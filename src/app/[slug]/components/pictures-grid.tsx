c7ae759a32d3997810b271083ef3fc989261ed3f
nachef <nathanvic.2001@gmail.com>
2025-03-13 22:31:54 -0300

    feat: update PicturesGrid component to display child media images dynamically

--- src/app/[slug]/components/pictures-grid.tsx
+++ src/app/[slug]/components/pictures-grid.tsx
@@ -7,7 +7,7 @@ import { getFormattedAge } from '../../../lib/helpers/formatters/birth_date_form
 import { ChildProps, MediaProps } from '../../../typings/child'
 
 interface PicturesGridProps {
- child: ChildProps
+  child: ChildProps
 }
 
 const dancing = Dancing_Script({
@@ -16,9 +16,11 @@ const dancing = Dancing_Script({
 })
 
 export default function PicturesGrid({ child }: PicturesGridProps) {
-
   const t = useTranslations()
   const { value, unit } = getFormattedAge(t, child?.birth_date)
+
+  // Pegando as imagens da mídia do child
+  const images = child.media || []
 
   return (
     <div className='relative w-full max-w-2xl mx-auto p-6'>
@@ -33,44 +35,74 @@ export default function PicturesGrid({ child }: PicturesGridProps) {
 
       {/* Photo Grid */}
       <div className='grid grid-cols-12 gap-2'>
-        {/* Main large photo */}
+        {/* Foto principal (maior) */}
         <div className='col-span-7 row-span-2'>
           <div className='relative w-full h-full min-h-[250px]'>
-            <Image src='/placeholder.svg?height=400&width=400' alt='Baby main photo' fill className='object-cover' />
+            <Image
+              src={images[0]?.url}
+              alt={images[0]?.id}
+              fill
+              className='object-cover transition-transform duration-300 ease-in-out hover:scale-110 hover:z-10'
+            />
           </div>
         </div>
 
-        {/* Top right photos */}
+        {/* Demais fotos distribuídas na grade */}
         <div className='col-span-5'>
           <div className='relative w-full h-full min-h-[120px]'>
-            <Image src='/placeholder.svg?height=200&width=200' alt='Baby photo 2' fill className='object-cover' />
+            <Image
+              src={images[1]?.url}
+              alt={images[1]?.id}
+              fill
+              className='object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:z-10'
+            />
           </div>
         </div>
+
         <div className='col-span-5'>
           <div className='relative w-full h-full min-h-[120px]'>
-            <Image src='/placeholder.svg?height=200&width=200' alt='Baby photo 3' fill className='object-cover' />
+            <Image
+              src={images[2]?.url}
+              alt={images[2]?.id}
+              fill
+              className='object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:z-10'
+            />
           </div>
         </div>
 
-        {/* Bottom left photos */}
-        <div className='col-span-4'>
+        {/* Demais fotos distribuídas na grade */}
+        <div className='col-span-5'>
           <div className='relative w-full h-full min-h-[120px]'>
-            <Image src='/placeholder.svg?height=200&width=200' alt='Baby photo 4' fill className='object-cover' />
+            <Image
+              src={images[3]?.url}
+              alt={images[3]?.id}
+              fill
+              className='object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:z-10'
+            />
           </div>
         </div>
-        <div className='col-span-4'>
-          <div className='relative w-full h-full min-h-[120px]'>
-            <Image src='/placeholder.svg?height=200&width=200' alt='Baby photo 5' fill className='object-cover' />
-          </div>
-        </div>
-
-        {/* Bottom right large photo */}
-        <div className='col-span-4 row-span-2'>
+        <div className='col-span-7 row-span-2'>
           <div className='relative w-full h-full min-h-[250px]'>
-            <Image src='/placeholder.svg?height=400&width=300' alt='Baby photo 6' fill className='object-cover' />
+            <Image
+              src={images[4]?.url}
+              alt={images[4]?.id}
+              fill
+              className='object-cover transition-transform duration-300 ease-in-out hover:scale-110 hover:z-10'
+            />
+          </div>
+        </div>
+        <div className='col-span-5'>
+          <div className='relative w-full h-full min-h-[120px]'>
+            <Image
+              src={images[5]?.url}
+              alt={images[5]?.id}
+              fill
+              className='object-cover transition-transform duration-300 ease-in-out hover:scale-105 hover:z-10'
+            />
           </div>
         </div>
       </div>
     </div>
   )
 }
+
